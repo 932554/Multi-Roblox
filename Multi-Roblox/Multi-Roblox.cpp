8e4ef9626314bc13254a23d81f9a2e05a094f1a2
932554 <61924694+932554@users.noreply.github.com>
2022-02-05 11:01:06 -0500

    Update Multi-Roblox.cpp

--- Multi-Roblox/Multi-Roblox.cpp
+++ Multi-Roblox/Multi-Roblox.cpp
@@ -6,7 +6,7 @@ int main()
     SetConsoleTitleA("Multi-Roblox");
 
     std::cout << "You will no longer be limited to one Roblox instance" << std::endl
-        << "Closing this will terminate all but one Roblox process" << std::endl;
+        << "Closing this will terminate all but one Roblox instance" << std::endl;
 
     CreateMutexA(nullptr, true, "ROBLOX_singletonMutex");
 
